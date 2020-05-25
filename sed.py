e689bd4a432a7fcba37290aef7ca98741db53977
IA <49097256+itayatas@users.noreply.github.com>
2020-05-25 19:53:30 +0300

    added validation of function tester

--- sed.py
+++ sed.py
@@ -6,18 +6,28 @@ __email__ = "itayatas10@gmail.com"
 __status__ = "Production"
 
 import re
-
-
 # re - Regular Expression is the basic usage of "SED" command.
+import sys
+
+# Import sys for getting argument via console
 
 # /g == global, all words in textfile / string
 # /i == ignore cases (upper, lower)
 # /p == printing the string / text file
 # /w == specific file
 
+# Reading arguments from command line, removing the first "sed.py" argument.
+
+script_arguments = sys.argv[1:]
+print(script_arguments)
+if not script_arguments:
+    sys.exit("Running this script via command-line only. Example: python 'sed s/...")
+if script_arguments[0] != "sed":
+    sys.exit("Please enter valid 'sed' function. Example: [sed 's/on/forward/' sample.txt] ")
+
 
 class Sed:
     def __init__(self, *args, old_file=None, new_file=None):
         """
-            
+
         """
