25ede3d28f92a27ad20106774032f143535cf5cd
IA <49097256+itayatas@users.noreply.github.com>
2020-05-25 22:15:24 +0300

    changeing strctuor using parser

--- sed.py
+++ sed.py
@@ -5,12 +5,15 @@ __version__ = "1.0.0"
 __email__ = "itayatas10@gmail.com"
 __status__ = "Production"
 
+import argparse
+import os
 import re
-# re - Regular Expression is the basic usage of "SED" command.
 import sys
 
 
+# re - Regular Expression is the basic usage of "SED" command.
 # Import sys for getting argument via console
+# argparse module add much more functional to command line/
 
 # /g == global, all words in textfile / string
 # /i == ignore cases (upper, lower)
@@ -20,25 +23,32 @@ import sys
 # Reading arguments from command line, removing the first "sed.py" argument.
 
 class Sed:
+    """
+    Sed class is implementation of 'sed' command in Linux environment.
+    This class initialize the functional of 'set' command by detecting flags and input/output files/strings
+    """
+
     def __init__(self, arguments):
         """
+
         """
+        # Finding the pattern in function. expecting "S" or "s" for replacing.
+        self.function = re.compile('\w+').findall(arguments[0])
+        self.sorce = arguments[1:]
+        print(self.sorce)
+        # if self.function[0].lower == "s":
+        print(self.function)
 
 
-script_arguments = sys.argv[1:]
-print(script_arguments)
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('sed', metavar='', type=str, help="This is implantation of Linux-'sed' command in python.")
+parser.add_argument('--sum', dest='accumulate', action='store_const',const=sum, default=max, help='sum the integers (default: find the max)')
 
-# If not found any argmuents, exit script with printed error.
-if not script_arguments:
-    sys.exit("Running this script via command-line only. Example: python 'sed s/...")
-
-# If script 'sed' function has wrong, exit script with printed error.
-if script_arguments[0] != "sed":
-    sys.exit("Please enter valid 'sed' function. Example: [sed 's/on/forward/' sample.txt] ")
-
-# If the 'sed' function is valid. (Deleting the word 'sed' from arguments)
+# checking if the sed is execute via "pipe", meaning a string incoming.
+if not os.isatty(0):
+    cmd = sys.stdin.read()
 else:
-    script_arguments = script_arguments[1:]
-    
-print(script_arguments)
-sed = Sed(script_arguments)
+    cmd = 0
+
+# Initialize new SED object. future features to detect different flags.
+# sed = Sed(script_arguments)
