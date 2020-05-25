5dc95b69b73f85d046d238832dd4438c6dc5ecb7
IA <49097256+itayatas@users.noreply.github.com>
2020-05-25 20:06:37 +0300

    another valid argmuent checker. initlize a class

--- sed.py
+++ sed.py
@@ -9,6 +9,7 @@ import re
 # re - Regular Expression is the basic usage of "SED" command.
 import sys
 
+
 # Import sys for getting argument via console
 
 # /g == global, all words in textfile / string
@@ -18,16 +19,26 @@ import sys
 
 # Reading arguments from command line, removing the first "sed.py" argument.
 
+class Sed:
+    def __init__(self, arguments):
+        """
+        """
+
+
 script_arguments = sys.argv[1:]
 print(script_arguments)
+
+# If not found any argmuents, exit script with printed error.
 if not script_arguments:
     sys.exit("Running this script via command-line only. Example: python 'sed s/...")
+
+# If script 'sed' function has wrong, exit script with printed error.
 if script_arguments[0] != "sed":
     sys.exit("Please enter valid 'sed' function. Example: [sed 's/on/forward/' sample.txt] ")
 
-
-class Sed:
-    def __init__(self, *args, old_file=None, new_file=None):
-        """
-
-        """
+# If the 'sed' function is valid. (Deleting the word 'sed' from arguments)
+else:
+    script_arguments = script_arguments[1:]
+    
+print(script_arguments)
+sed = Sed(script_arguments)
