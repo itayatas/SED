db2c8e76b142593483247a7177e1fd95c58a0245
IA <49097256+itayatas@users.noreply.github.com>
2020-05-26 21:09:44 +0300

    fixed checkers

--- sed.py
+++ sed.py
@@ -39,21 +39,44 @@ class Sed:
         # if self.function[0].lower == "s":
         print(self.function)
 
+
 def sedChecker(string):
+    """
+    @param string: The entered value. accept: 'sed' only.
+    @raise
+    """
     if string != 'sed':
         raise argparse.ArgumentTypeError("%s is not excepted. Please add 'sed'." % string)
     return string
 
+
 def expressionChecker(string):
+    """
+    @param string: The entered value. accept valid expression only. Example: 's/old_word/new_word/'
+    @raise
+    """
     if string != 'exp':
         raise argparse.ArgumentTypeError("'%s' is not valid expression. Please write valid expression." % string)
     return string
 
+
 def inputFileChecker(string):
-    return
+    """
+    @param string: The entered value. Checking if input_file OR 'string' via 'PIPE' entered. if not, raise error.
+    @If the found file, returns the name of the file. else: 
+    """
+    if os.path.isfile('%s' % (string)):
+        return string
+    else:
+        if not os.isatty(0):
+            return sys.stdin.read()
+        else:
+            cmd = 0
+        return
+
 
 def outputFileChecker(string):
-    if os.path.isfile('%s'%(string)):
+    if os.path.isfile('%s' % (string)):
         return
     else:
         open('%s' % string, "a")
@@ -61,21 +84,31 @@ def outputFileChecker(string):
 
 
 # Using parser module for extra functionals in command lines.
-# argparse.ArgumentParser is the "command" and description of it. auto added -h flag. | usage = initialize expression
+# argparse.ArgumentParser is the "command" and description of it. auto added -h flag.
+# foo = 'sed' function. metavar == 'sed' -> shows in command line.
+# expression = '/s/old_word/new_word'. metavar == 'expression' -> shows in command line.
+# input_file = the file will be modified. metavar == 'input_file' -> shows in command line.
+#   IF NOT ASSIGN: require 'pipe' data. (echo, cat, etc...)
+# output_file = the file will initialize with new data. metavar == 'output_file' -> shows in command line.
+#   IF NOT ASSIGN: the changes will be printed to command line.
+
 parser = argparse.ArgumentParser(description="This is implementation of 'sed' command in linux")
-parser.add_argument('foo',type=sedChecker, metavar='sed', help="The linux command will be execute once assigned to command line. Accepting 'S' attribute: to string change/")
-parser.add_argument('expression', type=expressionChecker, metavar="'expression'", help="Expression represent the command 'sed' use. "
-                                                                     "Example: 's/old_word/new_word/' ")
-parser.add_argument('input_file', type=inputFileChecker, metavar='input_file', help="The file 'sed' will extract data from. \n If argument not specfied: accept 'pipe' string input.")
-parser.add_argument('output_file', type=outputFileChecker, metavar='output_file', help="The file 'sed' write the changes to.\n If argument not specfied: the line will be printed to command line.")
+parser.add_argument('foo', type=sedChecker, metavar='sed',
+                    help="The linux command will be execute once assigned to command line. Accepting 'S' attribute: "
+                         "to string change/")
+parser.add_argument('expression', type=expressionChecker, metavar="'expression'",
+                    help="Expression represent the command 'sed' use. "
+                         "Example: 's/old_word/new_word/' ")
+parser.add_argument('input_file', type=inputFileChecker, metavar='input_file',
+                    help="The file 'sed' will extract data from. \n If argument not specified: accept 'pipe' string "
+                         "input.")
+parser.add_argument('output_file', type=outputFileChecker, metavar='output_file',
+                    help="The file 'sed' write the changes to.\n If argument not specified: the line will be printed "
+                         "to command line.")
 args = parser.parse_args()
 
-
 # checking if the sed is execute via "pipe", meaning a string incoming.
-if not os.isatty(0):
-    cmd = sys.stdin.read()
-else:
-    cmd = 0
+
 print(cmd)
 # Initialize new SED object. future features to detect different flags.
 # sed = Sed(script_arguments)
